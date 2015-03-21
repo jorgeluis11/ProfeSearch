bd8b020317d064d441c90705705b0958080f818c
jorgeluis11 <perez.jorge718@gmail.com>
2015-03-21 00:41:29 -0400

    search animations

--- www/js/directives/professors-list-directive.js
+++ www/js/directives/professors-list-directive.js
@@ -11,7 +11,8 @@ return{
 	scope:false,
 	templateUrl: 'templates/professors/view.html'
 }
-}).directive('hamburger', ['$ionicModal',function ($ionicModal) {
+}).directive('hamburger', ['$ionicModal',function ($ionicModal) 
+{
 	return {
 		restrict: 'E',
 		scope:{},
@@ -54,3 +55,44 @@ return{
 		}
 	};
 }])
+.directive("close", [function(){
+	return {
+		restrict:"A",
+		scope:false,
+		link:function(scope, element, attrs){
+			$(element).click(function(){
+				scope.query = "";
+				console.log(scope.query);
+			});
+
+		}
+	}
+}]).directive('searchAnimationEvent', [function () {
+	return {
+		restrict: 'A',
+		scope:{},
+		link: function (scope, element, attrs) {
+
+			scope.activate = false;
+			var target = $(element);
+			target.click(function(){
+				target.activate = !target.activate;
+				$("#modal-black-background").css("display","block");
+				$("#burger-container").css("z-index","0");
+				target.css("z-index","99999").find(".search-bar-size").removeClass("search-bar-size-more").addClass("search-bar-size-less");
+				$("#cancel-search").fadeIn(400);
+
+				$("#modal-black-background,#cancel-search").click(function(){
+					if(window.cordova && window.cordova.plugins.Keyboard)
+				    {
+				      window.cordova.plugins.Keyboard.close();
+				    }
+					target.css("display:none;")
+					target.css("z-index","0").find(".search-bar-size").removeClass("search-bar-size-less").addClass("search-bar-size-more");
+					$("#cancel-search").fadeOut(400);
+					$("#modal-black-background").css("display","none");
+				});
+			})
+		}
+	};
+}]);
